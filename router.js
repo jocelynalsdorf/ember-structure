1e1b9a86bae1457b49988dc6b6b501bb88933762
david garber & jocelyn alsdorf <student@epicodus.com>
2015-06-08 11:24:45 -0700

    adds controllers

--- router.js
+++ router.js
@@ -1,6 +1,10 @@
 Blogger.Router.map(function() {
   this.resource('posts', {path: '/'});
   this.resource('about');
-  this.resource('contact');
+  this.resource('contact', function() {
+    this.resource('phone');
+    this.resource('email');
+  });
+
 
 });
