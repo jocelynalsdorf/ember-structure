a42724f0841896063effd55671d38cdd1f8c99ba
david garber & jocelyn alsdorf <student@epicodus.com>
2015-06-08 11:43:38 -0700

    adds controller functionality

--- controllers/contact.js
+++ controllers/contact.js
@@ -3,6 +3,7 @@ Blogger.ContactController = Ember.Controller.extend({
   actions: {
     sendMessage: function() {
       var message = prompt('Type your message here:');
+      this.set('confirmationNumber', Math.round(Math.random() * 100000));
       this.set('messageSent', true);
     }
   }
