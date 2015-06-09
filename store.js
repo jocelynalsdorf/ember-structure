7027928a22433e50cef4206cb3ba717753b05dc0
jocelynalsdorf <jfrodsla@gmail.com>
2015-06-09 15:00:49 -0700

    adds sort array methods

--- store.js
+++ store.js
@@ -1,15 +1,17 @@
 var posts = [
 
+
+		{
+		id: '2',
+		title: 'When you are scared of the dark and a vampire',
+		body: "Dinah my dear! I wish you would seem to have any rules in particular; at least, if there were ten of them, and just as well as I do,' said the Queen, who were giving it something out of sight: 'but it sounds uncommon nonsense.' Alice said very politely, 'if I had it written down: but I shall be late!' (when she thought at first she would feel with all their simple sorrows, and find a thing,' said the Mock Turtle, and to stand on your head - Do you think you're changed, do you?' 'I'm afraid I've offended it again!' For the Mouse was swimming away from her as she spoke."
+	},
 	{
 		id: '1',
 		title: 'I love blood',
 		body: "They very soon had to do that,' said the White Rabbit read out, at the Duchess to play croquet.' The Frog-Footman repeated, in the schoolroom, and though this was her turn or not. So she went on muttering over the fire, and at last she stretched her arms folded, frowning like a serpent. She had just begun 'Well, of all the while, and fighting for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be a Caucus-race.' 'What IS the use of this elegant thimble'; and, when it saw Alice. It looked good-natured, she thought: still it was the cat.) 'I hope they'll remember her saucer of milk at tea-time."
 	},
-	{
-		id: '2',
-		title: 'When you are scared of the dark and a vampire',
-		body: "Dinah my dear! I wish you would seem to have any rules in particular; at least, if there were ten of them, and just as well as I do,' said the Queen, who were giving it something out of sight: 'but it sounds uncommon nonsense.' Alice said very politely, 'if I had it written down: but I shall be late!' (when she thought at first she would feel with all their simple sorrows, and find a thing,' said the Mock Turtle, and to stand on your head - Do you think you're changed, do you?' 'I'm afraid I've offended it again!' For the Mouse was swimming away from her as she spoke."
-	},
+	
 	{
 		id: '3',
 		title: 'This is not the End',
