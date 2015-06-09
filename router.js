ce2960eeeaa44e79f9eed851d79c573527e18e54
jocelynalsdorf <jfrodsla@gmail.com>
2015-06-09 14:02:38 -0700

    adds links to individual post by params.post_id

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
 Blogger.Router.map(function() {
   this.resource('posts', {path: '/'});
+  this.resource('recent-comments');
   this.resource('about');
   this.resource('contact', function() {
     this.resource('phone');
     this.resource('email');
   });
+  this.resource('post', {path: '/:post_id'});
 
 
 });
